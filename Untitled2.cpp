fd085ad1e7e41a5cf6432a7840609c929954f71f
selvin <scanahuis1@miumg.edu.gt>
2023-11-01 21:05:11 -0600

    PROYECTO

---
+++ Untitled2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+using namespace std;
+
+const char *nombre_archivo = "diccionario.dat";
+
+struct diccionario {
+    char palabra[500];
+    char traduccion[500];
+    char funcionalidad[500];
+};
+
+void leer();
+void crear();
+void actualizar();
+void borrar();
+void traducirCodigo();
+void mostrarMenu(
